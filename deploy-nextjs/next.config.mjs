79cb29aec8969b04509b60d2a25e4f8c03f01c7c
Arthur Nilo de Tassis <arthur.nilo@marcaambiental.com.br>
2026-07-03 11:05:42 -0300

    ajeitando deploy pages

--- deploy-nextjs/next.config.mjs
+++ deploy-nextjs/next.config.mjs
@@ -1,4 +1,16 @@
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+
+let basePath = "";
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+  basePath = `/${repo}`;
+}
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  basePath,
+  images: { unoptimized: true },
+};
 
 export default nextConfig;
